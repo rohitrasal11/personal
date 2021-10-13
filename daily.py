b0a826d0e3356afa3b9e870204d995183a384439
rohitrasal11 <rohit75870@gmail.com>
2021-10-13 10:27:26 +0530

    'add'

--- daily.py
+++ daily.py
@@ -1,7 +1,18 @@
-print('hello')
-'''
+# print('hello')
+
 def number(n):
     for i in range(1,11):
         print(f'{n}*{i}={n*i}')
-number(17)
-'''
+number(19)
+
+
+# import random
+# import time
+# import webbrowser
+
+# while True:
+#     sites = random.choice(['google.com','facebook.com','youtube.com'])
+#     visit = "https://{}".formate(sites)
+#     webbrowser.open(visit)
+#     seconds = random.randint(2,5)
+#     time.sleep(seconds)
